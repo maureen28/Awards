f278e1bc458875626f78f2796e475d1ae0835871
Maureen <murimimaureen69@gmail.com>
2020-06-06 22:42:55 +0300

    Create the profile display function

--- awwards/views.py
+++ awwards/views.py
@@ -49,3 +49,18 @@ def get_project(request, id):
         raise Http404()
     return render(request, "projects.html", {"project":project})
 
+@login_required(login_url='/accounts/login/')
+def profile_display(request):
+    current_user = request.user
+    author = current_user
+    projects = Project.get_by_author(author)
+    if request.method == 'POST':
+        form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.save()
+        return redirect('profile')
+    else:
+        form = ProfileUpdateForm()
+    return render(request, 'registration/profile.html', {"form":form, "projects":projects})
+
