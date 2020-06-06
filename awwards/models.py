6194ffa558f73c6cb7a0fec70a2df9f2f9c5a2f2
Maureen <murimimaureen69@gmail.com>
2020-06-06 13:50:05 +0300

    Add profile with user

--- awwards/models.py
+++ awwards/models.py
@@ -2,13 +2,16 @@ from django.db import models
 from django.contrib.auth.models import User
 from pyuploadcare.dj.models import ImageField
 from django_countries.fields import CountryField
+from star_ratings.models import Rating
+from django.db.models import ObjectDoesNotExist
+
 
 # Create your models here.
 
 class Profile(models.Model):
     profile_pic = models.ImageField()
     bio = models.CharField(max_length=100)
-    user = models.ForeignKey(User,blank=True, on_delete=models.CASCADE, related_name="profile")
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.bio
