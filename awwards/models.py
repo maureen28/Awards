cbe29f1ebea1467484f8f7596c150cbe2fde5aca
Maureen <murimimaureen69@gmail.com>
2020-06-06 13:47:56 +0300

    Add country field

--- awwards/models.py
+++ awwards/models.py
@@ -1,5 +1,22 @@
 from django.db import models
 from django.contrib.auth.models import User
 from pyuploadcare.dj.models import ImageField
+from django_countries.fields import CountryField
 
 # Create your models here.
+
+class Profile(models.Model):
+    profile_pic = models.ImageField()
+    bio = models.CharField(max_length=100)
+    user = models.ForeignKey(User,blank=True, on_delete=models.CASCADE, related_name="profile")
+
+    def __str__(self):
+        return self.bio
+
+    #Save profile
+    def profile_save(self):
+        self.save()
+
+     #delete profile
+    def delete_profile(self):
+        self.delete()
