4c943787e27574371b71790bd9aa3560428d2d91
Maureen <murimimaureen69@gmail.com>
2020-06-06 23:01:32 +0300

    Create the profile template

--- awwards/urls.py
+++ awwards/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import url
-from  awwards.views import home, about, search_results,new_project
+from  awwards.views import home, about, search_results,new_project, profile_display
 
 # Add your url patterns here
 
 urlpatterns = [
     url(r'about/', about, name='about'),
-    url(r'newproject/', new_project, name='newproject'),
+    url(r'new/', new_project, name='new'),
     url(r'search/', search_results, name='search_results'),
+    url(r'profile/', profile_display, name='profile'),
     url(r'', home, name='home'),
 
 ]
